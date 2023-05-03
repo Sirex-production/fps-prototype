aea69874a042119418624d785919c05f5081ac37
CheeseCake <grabarzpolski@gmail.com>
2023-05-03 19:29:08 +0100

    bleeding effect added

--- Assets/Source/Ingame/Ai/SpecialActions/TeleportationSpecialAction.cs
+++ Assets/Source/Ingame/Ai/SpecialActions/TeleportationSpecialAction.cs
@@ -7,13 +7,13 @@ namespace Ingame.Ai.SpecialActions
     
     public sealed class TeleportationSpecialAction : SpecialActionBase
     {
-
+        [SerializeField] private Transform model;
         protected override void Act()
         {
-            var enemyTransform = aiBaker.NavMeshAgent.transform;
+            var enemyTransform = model;
             var newPosition = enemyTransform.position + enemyTransform.forward * Random.Range(-5, 5) + enemyTransform.right * Random.Range(-5, 5);
-            
-            enemyTransform.LookAt(aiBaker.Entity.aiContextMdl.player);
+          //  enemyTransform.LookAt(aiBaker.Entity.aiContextMdl.player);
+            //enemyTransform.rotation = Quaternion.Euler(0,enemyTransform.rotation.y,0);
             enemyTransform.position = newPosition;
             aiBaker.NavMeshAgent.Warp(newPosition);
         }
